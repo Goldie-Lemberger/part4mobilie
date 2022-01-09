a8e7176e108c88cd80240c30c791e43eb51b6c59
goldie-lemberger <goldie9912@gmail.com>
2022-01-10 00:11:05 +0200

    fix paths

--- ModelFrame/Parts/distance_tfl.py
+++ ModelFrame/Parts/distance_tfl.py
@@ -9,10 +9,10 @@ class Distance_TFL:
 
     def run(self, path, prev_frame, tfls,tfl_points, currentEm,focal,pp):
 
-        prev_container = Frame_Model(path)
-        curr_container = Frame_Model(prev_frame)
-        prev_container.traffic_light = np.array(tfls)
-        curr_container.traffic_light = np.array(tfl_points)
+        prev_container = Frame_Model(prev_frame)
+        curr_container = Frame_Model(path)
+        prev_container.traffic_light = np.array(tfl_points)
+        curr_container.traffic_light = np.array(tfls)
         curr_container.EM = currentEm
         curr_container.corresponding_ind = SFM.calc_TFL_dist(prev_container, curr_container, focal, pp)
         return curr_container
